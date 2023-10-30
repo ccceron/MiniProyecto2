56280ce6457088676a003b9b561a6703573a299e
JuanFDJ73 <144862881+JuanFDJ73@users.noreply.github.com>
2023-10-30 00:46:08 -0500

    MVC (Esquema)
    Falta muchos cambios.

---
+++ src/Modelo/ModeloJuego.java
@@ -0,0 +1,17 @@
+package Modelo;
+
+public class ModeloJuego {
+    private String nombreUsuario;
+
+    public ModeloJuego() {
+        // Constructor del modelo si es necesario
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+}
